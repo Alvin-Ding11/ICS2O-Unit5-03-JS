905bd00ccd029ba1214048a2097694247b8ca42a
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-17 13:16:40 +0000

    minor js and html fixes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById('enter').addEventListener('click',enterResults)
+document.getElementById('enter').addEventListener('click', enterResults)
 
 function enterResults () {
   alert(document.getElementById('Poke').value)
 }
+// This function is triggered by the event listener to take the text from the textbox and to show it in an alert once the user clicks on the "Enter your Pokémon" button.
